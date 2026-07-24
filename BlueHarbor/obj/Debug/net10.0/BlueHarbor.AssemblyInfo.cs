2f105131b3d28dcf5b7cc342dd1d4120ea533cf4
Leonida Qemeqi <leonida.qemeqi@edu-its.it>
2026-07-24 14:22:03 +0200

    fix: update project paths in configuration files to reflect new directory structure

--- BlueHarbor/obj/Debug/net10.0/BlueHarbor.AssemblyInfo.cs
+++ BlueHarbor/obj/Debug/net10.0/BlueHarbor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlueHarbor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae06662e62dddf545627964d7602b7548727a130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d02588bbb5d9e82a823629220a66781d2ac6d40")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlueHarbor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlueHarbor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
